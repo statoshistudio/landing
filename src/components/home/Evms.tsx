cd175a603b1f875e221689c1ed879fb488800946
tauhidulislam100 <tauhidulislam100@gmail.com>
2023-02-28 21:30:23 +0600

    fixed responsive issue

--- src/components/home/Evms.tsx
+++ src/components/home/Evms.tsx
@@ -32,30 +32,33 @@ const Evms = () => {
       <div id="evms-title">
         <h2
           data-animate
-          className="text-2xl xs:text-3xl sm:text-4xl md:text-[40px] font-bold"
+          className="text-2xl xs:text-3xl sm:text-4xl md:text-[40px] text-center sm:text-left font-bold"
         >
           Top Verified Collections from Popular EVMs
         </h2>
-        <p data-animate className="text-lg text-[#A9A9C2] mt-2">
+        <p
+          data-animate
+          className="text-lg text-[#A9A9C2] mt-2 text-center sm:text-left"
+        >
           The most well-known Collection - Based on the last 30 days{' '}
         </p>
       </div>
       <div
         id="evms"
-        className="mt-16 md:mt-20 flex flex-wrap justify-center gap-5 2xl:gap-10"
+        className="mt-16 md:mt-20 grid grid-cols-1 md:grid-cols-2 gap-5 2xl:gap-10"
       >
         {collectoinsData.map((collection, index) => (
           <div
             data-animate
             key={index}
-            className="max-w-full flex-1 px-4 py-[34px] cursor-pointer border border-transparent hover:border-primary duration-300 flex items-center gap-2.5 rounded-xl bg-gradient-to-t to-[#70707266] from-[#64617126]"
+            className="flex-1 px-4 py-[34px] cursor-pointer border border-transparent hover:border-primary duration-300 flex items-center gap-2.5 rounded-xl bg-gradient-to-t to-[#70707266] from-[#64617126]"
           >
-            <div className="min-w-[40px] min-h-[40px] sm:w-[50px] sm:h-[50px] relative bg-[#565660] rounded-full">
+            <div className="min-w-[40px] min-h-[40px] sm:min-w-[50px] sm:min-h-[50px] sm:w-[50px] sm:h-[50px] relative bg-[#565660] rounded-full">
               <BlueStar className="absolute -bottom-1 -right-1" />
             </div>
             <div className="">
               <h4 className="font-bold">{collection.title}</h4>
-              <p className="text-xs font-medium mt-1 whitespace-normal break-all">
+              <p className="text-xs font-medium mt-1 break-all">
                 {collection.cryptoAddress}
               </p>
             </div>
